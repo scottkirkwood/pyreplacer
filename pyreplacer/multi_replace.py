c16de4c60a4748cf339d8deec4d976d5fcaed88d
Scott Kirkwood <scott@forusers.com>
2014-01-19 17:50:37 -0500

    Multi replace test

--- pyreplacer/multi_replace.py
+++ pyreplacer/multi_replace.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python
 
 import re
+import types
 
 class MultiReplace(object):
     """An all-in-one multiple string substitution class."""
     def __init__(self, config):
-        self.re = None
-        self.regex = None
         self.config = config
+        self.from_to = []
         self.compile()
 
     def compile(self):
         """ Build a regular expression object based on the keys of
             the current dictionary."""
-        for index, from_to in enumerate(self.config['from_to']):
-            from_to['compiled_from'] = re.compile(from_to['from'])
+        self.from_to = []
+        for ft in self.config['from_to']:
+            self.from_to.append((re.compile(ft['from']), ft['to']))
+
+    def replacer(self, to):
+        """Function closure to create handle all replace text."""
+        def rep(match):
+            if not isinstance(to, basestring):
+                # could do something with groups here.
+                return to(match.group(0))
+            return match.expand(to)
+        return rep
 
     def sub(self, s):
         """Translate text, returns the modified text."""
-        for from_to in self.config['from_to']:
-            s = from_to['compiled_from'].sub(from_to['to'], s)
+        for re_from, to in self.from_to:
+            s = re_from.sub(self.replacer(to), s)
         return s
-
-#
-# Test
-#
-if __name__ == '__main__':
-    text = 'Larry Wall is the creator of Perl'
-    config = {
-        'from_to': [
-            {
-                'from': r'L.+ W.l+',
-                'to': 'Guido van Rossum',
-            }, {
-                'from': 'creator',
-                'to':'Benevolent Dictator for Life'
-            }, {
-                'from': 'Perl',
-                'to': 'Python'
-            },
-        ],
-    }
-    sub = MultiReplace(config)
-    new = sub.sub(text)
-    print new
